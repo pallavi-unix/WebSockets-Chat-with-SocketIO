f91f320bb290393fbeda8670a7a774d260bd81c1
Pallavi Deshmukh <pallavideshmukh32897@gmail.com>
2023-11-10 11:52:31 -0500

    Complete the basic Assignment 13

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
 });
 
 server.listen(3000, () => {
